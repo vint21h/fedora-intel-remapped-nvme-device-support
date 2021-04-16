a139a9a13f691e4d52f5433d26aad18892ef305e
Igor Zubkov <igor.zubkov@gmail.com>
2021-04-16 17:14:19 +0300

    Use pipefail

--- build-kernel.sh
+++ build-kernel.sh
@@ -4,18 +4,18 @@
 # build-kernel.sh
 
 
-set -eaux pipefail;
+set -eaux pipefail
 
-sudo dnf install fedpkg fedora-packager rpmdevtools ncurses-devel pesign grubby \
-  && mkdir fedora-custom-kernel \
-  && cd fedora-custom-kernel \
-  && fedpkg clone -a kernel \
-  && cd kernel \
-  && git checkout -b local origin/f"${FEDORA_VERSION}" \
-  && sudo dnf builddep kernel.spec \
-  && wget -c "${PATCH_URL}" -O patch-intel-remapped-nvme-device-support.patch \
-  && sed -i 's/# define buildid .local/%define buildid .local/g' kernel.spec \
-  && sed -i '/^Patch1: patch-%{stableversion}-redhat.patch/a Patch2: patch-intel-remapped-nvme-device-support.patch' kernel.spec \
-  && sed -i '/^ApplyOptionalPatch patch-%{stableversion}-redhat.patch/a ApplyOptionalPatch patch-intel-remapped-nvme-device-support.patch' kernel.spec \
-  && fedpkg srpm \
-  && fedpkg local;\
+sudo dnf install fedpkg fedora-packager rpmdevtools ncurses-devel pesign grubby ; \
+  mkdir fedora-custom-kernel ; \
+  cd fedora-custom-kernel ; \
+  fedpkg clone -a kernel ; \
+  cd kernel ; \
+  git checkout -b local origin/f"${FEDORA_VERSION}" ; \
+  sudo dnf builddep kernel.spec ; \
+  wget -c "${PATCH_URL}" -O patch-intel-remapped-nvme-device-support.patch ; \
+  sed -i 's/# define buildid .local/%define buildid .local/g' kernel.spec ; \
+  sed -i '/^Patch1: patch-%{stableversion}-redhat.patch/a Patch2: patch-intel-remapped-nvme-device-support.patch' kernel.spec ; \
+  sed -i '/^ApplyOptionalPatch patch-%{stableversion}-redhat.patch/a ApplyOptionalPatch patch-intel-remapped-nvme-device-support.patch' kernel.spec ; \
+  fedpkg srpm ; \
+  fedpkg local
